9067782369abf6c3251bb1fef1b4b4043f327469
覃超成 <meimaonong@163.com>
2017-04-20 17:48:41 +0800

    模板提交

--- api/config/web.php
+++ api/config/web.php
@@ -46,6 +46,8 @@ $config = [
                 '<controller:\w+>/<id:\d+>'=>'<controller>/view',
                 '<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
                 '<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
+
+                ['class' => 'yii\rest\UrlRule', 'controller' => 'user'],
             ],
         ],
         
